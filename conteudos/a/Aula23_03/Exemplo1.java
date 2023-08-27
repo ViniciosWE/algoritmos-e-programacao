3cab506782256a0901ce2788d153d704f309e0f8
Vinicios  Weide Ebling <127418830+ViniciosWE@users.noreply.github.com>
2023-08-27 13:19:28 -0300

    Add files via upload

---
+++ conteudos/a/Aula23_03/Exemplo1.java
@@ -0,0 +1,11 @@
+
+package a.Aula23_03;
+
+public class Exemplo1 {
+    public static void main(String args[]){
+        int a = 6;
+        int b = 7;
+        int c = a + b;
+        System.out.println(c);        
+    }
+}
---
+++ conteudos/a/Aula23_03/Exercicio.java
@@ -0,0 +1,10 @@
+
+package a.Aula23_03;
+
+public class Exercicio {
+    public static void main(String args[]){
+        int num1 = 7;
+        int num2 = num1 * num1;
+        System.out.println("Cálculo do quadrado: " + num2);
+    }
+}
