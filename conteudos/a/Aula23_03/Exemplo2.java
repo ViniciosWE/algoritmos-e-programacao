a3795adb5f14e280968720e5d0ed90ec977a1ab4
Vinicios  Weide Ebling <127418830+ViniciosWE@users.noreply.github.com>
2023-08-27 13:13:54 -0300

    Add files via upload

---
+++ conteudos/a/Aula23_03/Exemplo2.java
@@ -0,0 +1,13 @@
+
+package a.Aula23_03;
+
+public class Exemplo2 {
+    public static void main(String args[]){
+        int a = 15;
+        int b = 8;
+        int c = a / b;
+        System.out.println("Quoc. divisão: " + c);
+        int d = a % b;
+        System.out.println("Resto divisão: " + d);
+    }
+}
---
+++ conteudos/a/Aula23_03/Exemplo3.java
@@ -0,0 +1,11 @@
+
+package a.Aula23_03;
+
+public class Exemplo3 {
+    public static void main(String args[]){
+        double a = 13.9;
+        double b = 5.7;
+        double c = a / b;
+        System.out.println("Resultado: " + c);
+    }
+}
